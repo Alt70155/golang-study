1675a1ac43855221031cc0c727b65b3e71b5b975
Alt70155 <chika1086@gmail.com>
2021-07-26 21:38:39 +0900

    rapidash導入

--- api-server/go.mod
+++ api-server/go.mod
@@ -4,11 +4,13 @@ go 1.16
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.knocknote.io/rapidash v0.3.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- api-server/server.go
+++ api-server/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gomodule/redigo/redis"
 	"github.com/labstack/echo"
+	"go.knocknote.io/rapidash"
 )
 
 type User struct {
@@ -15,16 +18,77 @@ type User struct {
 	PassHash  string `json:"passhash"`
 }
 
+func (u *User) DecodeRapidash(decoder rapidash.Decoder) error {
+	u.ID = decoder.Int("id")
+	u.Nickname = decoder.String("nickname")
+	u.LoginName = decoder.String("login_name")
+	u.PassHash = decoder.String("pass_hash")
+	return decoder.Error()
+}
+
+func (u User) RapidashStruct() *rapidash.Struct {
+	return rapidash.NewStruct("users").
+		FieldInt("id").
+		FieldString("nickname").
+		FieldString("login_name").
+		FieldString("pass_hash")
+}
+
+type UserSlice []*User
+
+func (e *UserSlice) DecodeRapidash(decoder rapidash.Decoder) error {
+	*e = make(UserSlice, decoder.Len())
+	for i := 0; i < decoder.Len(); i++ {
+		var user User
+		if err := user.DecodeRapidash(decoder.At(i)); err != nil {
+			return err
+		}
+		(*e)[i] = &user
+	}
+	return decoder.Error()
+}
+
 func main() {
+	// mysql
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/sample_db_go")
 	defer db.Close()
 	judgePanic(err)
 
+	// rapidash
+	// redis へのアドレスを指定して rapidash インスタンス作成
+	cache, err := rapidash.New(rapidash.ServerAddrs([]string{"127.0.0.1:6379"}))
+	judgePanic(err)
+	err = cache.WarmUp(db, new(User).RapidashStruct(), true)
+	judgePanic(err)
+	// *sql.Tx インスタンスの作成
+	txConn, err := db.Begin()
+	judgePanic(err)
+	cacheTx, err := cache.Begin(txConn)
+	judgePanic(err)
+
+	defer func() {
+		if err := cacheTx.RollbackUnlessCommitted(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	var users UserSlice
+
+	if err := cacheTx.FindByQueryBuilder(rapidash.NewQueryBuilder("users").Eq("id", 1), &users); err != nil {
+		log.Println(err)
+	}
+	// Commitしてキャッシュを更新する
+	if err := cacheTx.Commit(); err != nil {
+		log.Println(err)
+	}
+
+	cacheTx.Commit()
+
+	// api-server
 	e := echo.New()
-	user := show(db)
 
 	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, user)
+		return c.JSON(http.StatusOK, users)
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
@@ -43,4 +107,13 @@ func show(db *sql.DB) User {
 	judgePanic(err)
 
 	return user
+}
+
+func ConnectionRedis() redis.Conn {
+	const Addr = "127.0.0.1:6379"
+
+	c, err := redis.Dial("tcp", Addr)
+	judgePanic(err)
+
+	return c
 }
